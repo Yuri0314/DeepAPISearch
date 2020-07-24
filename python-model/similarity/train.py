8183ae90edc7cae7bb5330f0ab0b3fa35a7bc353
unknown <chenheng199898@gmail.com>
2020-07-24 11:37:27 +0800

    update model

--- python-model/similarity/train.py
+++ python-model/similarity/train.py
@@ -9,28 +9,32 @@ from predict import *
 def batch_loss(rnn_q,rnn_api,query,api,tag,loss,hidden_size, temperature = 0.3):
 #   初始化GRU的隐藏层状态
     w = torch.empty(3,query.shape[0],hidden_size).to(device)
-    init_state=torch.nn.init.orthogonal_(w, gain=1)
-    q_state=init_state
-    api_state=init_state
+    q_init_state=torch.nn.init.orthogonal_(w, gain=1)
+    q_state=q_init_state
+    api_init_state=torch.nn.init.orthogonal_(w, gain=1)
+    api_state=api_init_state
     q_output=rnn_q(query,q_state)
     api_output=rnn_api(api,api_state)
     #此时输出的维度猜测应该是（batch_size,hidden_size）
     #下方为AI研习社语义任务之后修改的向量维度
     #
-    # print(q_output.shape)
-    # print(api_output.shape)
+    # print(query)
+    # print(q_output)
+    # print('------------------------')
+    # print(api)
+    # print(api_output)
     q_output=q_output/torch.norm(q_output,dim=1).unsqueeze(1)
     api_output=api_output/torch.norm(api_output,dim=1).unsqueeze(1)
-
-    pre_score=torch.mm(q_output,api_output.permute(1,0))
+    pre_score=torch.sum(q_output*api_output,dim=1)
+    # pre_score=torch.mm(q_output,api_output.permute(1,0))
     # cls_score = torch.exp((q_output * api_output).sum(axis=1) / temperature)
     # exp_mat = torch.exp(pre_score / temperature)
     # prob = cls_score / exp_mat.sum(axis=1)
     #此处进行sigmoid方程与结果的概率进行（0-1）匹配。可能存在一部分问题，待确定
-    score=torch.sigmoid(torch.flatten(pre_score / temperature))
+    score=torch.sigmoid(pre_score / temperature)
     # score=torch.flatten((pre_score))#与下方损失函数相对应
     # score=(score+1)/2
-  #！！！！！这个l的初始化可能有问题！！！！
+
     #tag应该是一个1维向量（由对角矩阵拉直）
     l=loss(score,tag).mean()
     # l = loss(prob, tag)
@@ -54,15 +58,23 @@ def train(rnn_q,rnn_api,dataset,lr,batch_size,num_epochs,hidden_size,device):
     print('开始训练~~~~~~~~~~~~~')
     for epoch in range(num_epochs):
         data_iter=Data.DataLoader(dataset,batch_size,shuffle=True)
+
         ##print("epoch为：{}, iter: {}".format(epoch, len(data_iter)))
         l_sum=0.0
         for iter, (query,api) in enumerate(data_iter):
+            data_random=Data.DataLoader(dataset,batch_size,shuffle=True)
+            for  iter2,(query,_) in enumerate(data_random):
+                if iter2 > 0:
+                    break
+                query_random=query
+            api=torch.cat((api,api))
+            query=torch.cat((query,query_random))
             query=query.to(device)
             api=api.to(device)
             query_optimizer.zero_grad()
             api_optimizier.zero_grad()
             # res_tag=torch.flatten(torch.ones(query.shape[0]).to(device))
-            res_tag=torch.flatten(torch.eye(query.shape[0], query.shape[0]).to(device))
+            res_tag=torch.cat((torch.ones(int(api.shape[0]/2)),torch.zeros(int(api.shape[0]/2)))).to(device)
             l=batch_loss(rnn_q,rnn_api,query,api,res_tag,loss,hidden_size)
             l.backward()
             query_optimizer.step()
@@ -77,8 +89,8 @@ if __name__=='__main__':
     device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
     api_dict = {}
     #设置相关超参数
-    hidden_size,out_size = 512, 128
-    lr, batch_size, num_epochs =0.1, 2, 1
+    hidden_size,out_size = 512, 32
+    lr, batch_size, num_epochs =0.005, 2, 50
 
     #注意此时 res_tag的形状即为（batch_size,batch_size）
 
@@ -86,13 +98,13 @@ if __name__=='__main__':
     #假设此时的batch_size为2，应该是一个对角矩阵（对角线均为1.0）
     query_max_length=15
     #需要提前计算api数据集中api序列的最大长度
-    api_max_length=11
-    path="./11111.txt"
+    api_max_length=8
+    path="./1.txt"
     in_vocab,out_vocab,dataset=read_data(path,query_max_length,api_max_length,api_dict)
 
     print(in_vocab)
-    rnn_q = GRU(len(in_vocab),hidden_size,out_size).to(device)
-    rnn_api = GRU(len(in_vocab),hidden_size,out_size).to(device)
+    rnn_q = GRU(batch_size,len(in_vocab),hidden_size,out_size).to(device)
+    rnn_api = GRU(batch_size,len(in_vocab),hidden_size,out_size).to(device)
     train(rnn_q, rnn_api, dataset, lr, batch_size, num_epochs,hidden_size,device)
 
     print('训练阶段完毕。。。。。。。')
